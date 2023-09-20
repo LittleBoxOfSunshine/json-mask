8959ee7f71b4818fb27aae230ca01e1784ae60cb
Chris Henk <cahenk95@gmail.com>
2023-09-20 10:31:47 -0700

    documentation

--- src/lib.rs
+++ src/lib.rs
@@ -6,80 +6,49 @@
 //!
 //! <br>
 //!
-//! This library provides [`PollingTask`] and [`SelfUpdatingPollingTask`] structs for scheduling a
-//! closure to execute as a recurring task.
+//! This library provides [`mask::JsonMasker`] which accepts a [JSON Schema](https://json-schema.org/)
+//! document.
 //!
-//! It is common for a service to have long lived polling operations for the life of the process.
-//! The intended use case is to offer a RAII container for a polled operation that will interrupt
-//! pending sleeps to allow a low-latency clean exit.
-//!
-//! If the poll operation is still running, the task drop will join the background thread which will
-//! exit after the closure finishes.
+//! This is an early build where the input validation is more flexible. Based on real world usage
+//! the first stable release will probably restrict to formal schema draft standards and/or allow
+//! you to restrict to specific ones.
 //!
 //! # Examples
-//! - Use [`PollingTask`] to emit a heart beat every 30 seconds.
+//! - Use generate a mask with [`from_str`] or [`from_reader`] and apply it to a document.
 //!
 //!   ```
-//!   use interruptible_polling::PollingTask;
-//!   use std::time::Duration;
+//!   use json_mask::from_str;
+//!   use json_mask::JsonMasker;
+//!   use json_mask::ValidJsonSchema;
 //!
-//!   let task = PollingTask::new(Duration::from_secs(30), Box::new(|| {
-//!       println!("BeatBeat");
-//!   }));
-//!   ```
-//!
-//! - Some polled operations such as configuration updates contain the updated rate at which the
-//!   service should continue to poll for future updates. The [`SelfUpdatingPollingTask`] passes a
-//!   callback to the poll task that allows it to conveniently apply the new state to future polls.
-//!
-//!   ```no_run
-//!   use interruptible_polling::{PollingIntervalSetter, SelfUpdatingPollingTask};
-//!   use std::time::Duration;
-//!   use serde_json::{Value, from_reader};
-//!   use std::fs::File;
-//!   use std::io::BufReader;
-//!
-//!   let task = SelfUpdatingPollingTask::new(Duration::from_secs(30), Box::new(
-//!       move |setter: &PollingIntervalSetter| {
-//!           let file = File::open("app.config").unwrap();
-//!           let reader = BufReader::new(file);
-//!           let config: Value = from_reader(reader).expect("JSON was not well-formatted");
-//!
-//!           // Do other work with config
-//!
-//!           setter(Duration::from_secs(config["pollingInterval"].as_u64().unwrap()))
-//!               .expect("Polling interval isn't u64 convertable");
+//!   let mut document = serde_json::from_str(r#"{ "foo": 1, "bar": 2}"#).unwrap();
+//!   let schema = r#"
+//!   {
+//!     "$schema": "http://json-schema.org/draft-04/schema",
+//!     "title": "Demo Schema",
+//!     "description": "Demo",
+//!     "type": "object",
+//!     "properties": {
+//!       "foo": {
+//!         "type": "integer"
 //!       }
-//!   ));
+//!     }
+//!   }
+//!   "#;
+//!
+//!   let mask = from_str(schema).unwrap();
+//!   let masker = JsonMasker::new(mask);
+//!   masker.mask(&mut document);
+//!
+//!   assert_eq!(r#"{"foo":1}"#, serde_json::to_string(&document).unwrap())
 //!   ```
-//!
-//! - If your poll operation is long lived or internally iterative, there are opportunities to assert
-//!   if the task is still active to allow the blocked clean exit to occur faster. If you create the
-//!   task with [`PollingTask::new_with_checker`] or [`SelfUpdatingPollingTask::new_with_checker`]
-//!   your closure will receive a lookup function to peek if the managed task is still active. The
-//!   type alias [`StillActiveChecker`] defines the signature of the lookup function.
-//!
-//! ```
-//!  use interruptible_polling::{PollingTask, StillActiveChecker};
-//!  use std::time::Duration;
-//!
-//!  let task = PollingTask::new_with_checker(
-//!      Duration::from_secs(30),
-//!      Box::new(|checker: &StillActiveChecker|
-//!  {
-//!      let keys = vec![1 ,2, 3];
-//!
-//!      for key in keys {
-//!          // Early exit if signaled. The task will not poll again either way, but you have
-//!          // returned control to the parent task earlier.
-//!          if !checker() {
-//!              break;
-//!          }
-//!
-//!          // Some long or potentially long operation such as a synchronous web request.
-//!      }
-//!  }));
-//! ```
 //!
 
-pub mod mask;
+mod mask;
+
+pub use mask::JsonMasker;
+pub use mask::Mask;
+pub use mask::ValidJsonSchema;
+pub use mask::ParseError;
+pub use mask::from_str;
+pub use mask::from_reader;
